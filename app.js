a6112ce8d4497bb147943cbd0285995226a599e8
Rizky Anfasa Farras Mada <rizkyanfasafm@gmail.com>
2022-01-01 11:51:44 +0700

    limit middleware

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('Middleware');
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+    res.send("<form action='/product' method='POST'><input type='text' name='title'/><button type='submit'>Add Product</button></form>");
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 })
 
-app.use((req, res, next) => {
-    console.log('Another Middleware');
-    res.send("<h1>Hello from Express</h1>");
-})
+app.use('/', (req, res, next) => {
+    res.send("hello test");
+});
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
