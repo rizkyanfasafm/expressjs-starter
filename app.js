7168da4a6f7a567a3db40d3dae5571179a71e672
Rizky Anfasa Farras Mada <rizkyanfasafm@gmail.com>
2022-01-01 13:53:10 +0700

    handle routing 404

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(shopRoutes);
 app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page Not found</h1>');
+})
 
 app.listen(3000);
