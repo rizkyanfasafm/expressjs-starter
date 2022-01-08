bfdf1ba212d342adc0ef5450cab836adca156181
Rizky Anfasa Farras Mada <rizkyanfasafm@gmail.com>
2022-01-08 17:19:18 +0700

    working with ejs

--- app.js
+++ app.js
@@ -5,14 +5,17 @@ const path = require('path');
 
 const app = express();
 
+// EJS Configuration
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 // Handlebars Configuration
-app.engine('handlebars', expressHbs({
-    layoutsDir: 'views/layouts/',
-    defaultLayout: 'main-layout'
-}));
-app.set('view engine', 'handlebars');
-app.set('views', 'views');
+// app.engine('handlebars', expressHbs({
+//     layoutsDir: 'views/layouts/',
+//     defaultLayout: 'main-layout'
+// }));
+// app.set('view engine', 'handlebars');
+// app.set('views', 'views');
 
 // Pug Configuration
 // app.set('view engine', 'pug');
@@ -20,6 +23,7 @@ app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const { application } = require('express');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
