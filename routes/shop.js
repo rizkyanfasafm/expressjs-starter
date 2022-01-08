0ad006f12e3cb906d01dd15c16b1753eaa68cb97
Rizky Anfasa Farras Mada <rizkyanfasafm@gmail.com>
2022-01-08 11:04:04 +0700

    Convert to Handlebars

--- routes/shop.js
+++ routes/shop.js
@@ -13,7 +13,7 @@ router.get('/', (req, res, next) => {
     // Get path completely until routes
     // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 
-    res.render('shop', {prods: products, pageTitle: 'My Shop', path: '/'});
+    res.render('shop', {prods: products, pageTitle: 'My Shop', path: '/', hasProducts: products.length > 0});
 });
 
 module.exports = router;
