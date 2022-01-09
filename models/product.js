6ddede590bfea741cd57b59a35c54e112cccdaf3
Rizky Anfasa Farras Mada <rizkyanfasafm@gmail.com>
2022-01-09 10:49:55 +0700

    Refactoring file storage

--- models/product.js
+++ models/product.js
@@ -1,44 +1,48 @@
 const fs = require("fs");
 const path = require("path");
-const products = [];
+// const products = [];
+
+const p = path.join(
+  path.dirname(require.main.filename),
+  "data",
+  "products.json"
+);
+
+const getProductFromFile = (cb) => {
+  // Will be read fil
+  fs.readFile(p, (err, fileContent) => {
+    //   If error it indicates that file is not found or empty, so we'll return callback empty array
+    if (err) {
+      // Return callback empty array
+      cb([]);
+    } else {
+      // If file has a content, we'll return callback content from file
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
+  // Every calling class Product we must passing a value
   constructor(title) {
+    // And this value, will be save in temp memory
     this.title = title;
   }
 
   save() {
-    // products.push(this);
-    const p = path.join(
-      path.dirname(require.main.filename),
-      "data",
-      "products.json"
-    );
-
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    // If we want save a product, we must get all product from file, therefore we use getProductFromFile method
+    getProductFromFile((products) => {
+      // We must use callback for wait a process to read file
       products.push(this);
+      // products will generate a array that have a list of title and added title in temp memory to add this temp producst (array)
       fs.writeFile(p, JSON.stringify(products), (err) => {
+        // We want overwrite content in file product.js
         console.log(err);
       });
     });
   }
 
   static fetchAll(cb) {
-    const p = path.join(
-      path.dirname(require.main.filename),
-      "data",
-      "products.json"
-    );
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([]);
-      }
-    cb(JSON.parse(fileContent));
-    });
-    // return products;
+    getProductFromFile(cb);
   }
 };
